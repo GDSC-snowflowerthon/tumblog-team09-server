d6b60cfa569075908e6936ac566359d4ec0be0af
joonghyun <joongh@ewhain.net>
2024-01-09 11:06:38 +0900

    Initialize the project

---
+++ src/main/java/snowflake/tumblog/TumblogApplication.java
@@ -0,0 +1,13 @@
+package snowflake.tumblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TumblogApplication.class, args);
+    }
+
+}
