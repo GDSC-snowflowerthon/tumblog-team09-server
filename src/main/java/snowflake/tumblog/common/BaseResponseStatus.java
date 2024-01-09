3c5821b01dc6626f82bf97d0e558e6daa900441b
joonghyun <joongh@ewhain.net>
2024-01-09 15:16:57 +0900

    #3 infra: Base 파일 추가

---
+++ src/main/java/snowflake/tumblog/common/BaseResponseStatus.java
@@ -0,0 +1,43 @@
+package snowflake.tumblog.common;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000: 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    /**
+     * 2000: Request 오류
+     */
+    // user(2000~2099)
+    INVALID_USER_IDX(false, 2000, "잘못된 user Idx 입니다."),
+
+    // tumble(2100~2199)
+
+    /**
+     * 3000: Response 오류
+     */
+    // user(3000~3099)
+
+    // tumble(3100~3199)
+
+
+    /**
+     * 4000: DB, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패했습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
+
