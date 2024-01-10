f7994a40dcf95da3927907ebbffe57679babf87c
Geonwoo Pack <packdev937@gmail.com>
2024-01-10 23:21:56 +0900

    #18 test : Tumble이 정상적으로 조회되는지 테스트 한다

--- src/test/java/snowflake/tumblog/tumble/service/TumbleServiceTest.java
+++ src/test/java/snowflake/tumblog/tumble/service/TumbleServiceTest.java
@@ -1,5 +1,6 @@
 package snowflake.tumblog.tumble.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
@@ -10,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import snowflake.tumblog.common.BaseException;
+import snowflake.tumblog.tumble.domain.Size;
 import snowflake.tumblog.tumble.dto.CreateTumbleRequest;
+import snowflake.tumblog.tumble.dto.TumbleDetailResponse;
 import snowflake.tumblog.tumble.repository.TumbleRepository;
 import snowflake.tumblog.user.domain.User;
 import snowflake.tumblog.user.domain.repository.UserRepository;
@@ -32,10 +35,18 @@ class TumbleServiceTest {
     @BeforeEach
     void setUp() {
         userService.signup(CreateUserRequest.of("nickname"));
+        tumbleRepository.deleteAll();
     }
 
     @Test
-    void Tumble을_생성한다() throws BaseException {
+    void Tumble_생성() throws BaseException {
+        Tumble_생성_요청();
+
+        // then
+        assertThat(tumbleRepository.findAll()).hasSize(1);
+    }
+
+    private void Tumble_생성_요청() throws BaseException {
         // given
         CreateTumbleRequest request = new CreateTumbleRequest(LocalDate.of(2024, 01, 10), "아메리카노",
             3000, "M");
@@ -43,8 +54,20 @@ class TumbleServiceTest {
 
         // when
         tumbleService.create(request, userId);
+    }
+
+    @Test
+    void Tumble_조회() throws BaseException {
+        // given
+        Tumble_생성_요청();
+        Long tumbleId = 1L;
+
+        // when
+        TumbleDetailResponse response = tumbleService.detail(tumbleId);
 
         // then
-        Assertions.assertThat(tumbleRepository.findAll()).hasSize(1);
+        assertThat(response.menu()).isEqualTo("아메리카노");
+        assertThat(response.discountPrice()).isEqualTo(3000);
+        assertThat(response.size()).isEqualTo(Size.M);
     }
 }
