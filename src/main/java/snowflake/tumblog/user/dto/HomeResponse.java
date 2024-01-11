b9bc619ab4417b3691aeacdb757edaf048eaf9b4
Geonwoo Pack <packdev937@gmail.com>
2024-01-11 11:20:27 +0900

    #18 refactor : DTO 클래스를 Record 타입으로 리팩토링 한다

--- src/main/java/snowflake/tumblog/user/dto/HomeResponse.java
+++ src/main/java/snowflake/tumblog/user/dto/HomeResponse.java
@@ -2,42 +2,22 @@ package snowflake.tumblog.user.dto;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
-import snowflake.tumblog.tumble.domain.Tumble;
 import snowflake.tumblog.tumble.domain.Tumbles;
 import snowflake.tumblog.user.domain.User;
 
-public class HomeResponse {
-
-    private int annualSavedMoney;
-    private int annualSavedCarbon;
-    private List<TumbleResponse> monthlyTumbles;
-    private String Level;
-
-    private HomeResponse(int annualSavedMoney, int annualSavedCarbon,
-        List<TumbleResponse> monthlyTumbles, String level) {
-        this.annualSavedMoney = annualSavedMoney;
-        this.annualSavedCarbon = annualSavedCarbon;
-        this.monthlyTumbles = monthlyTumbles;
-        Level = level;
-    }
+public record HomeResponse(int annualSavedMoney, int annualSavedCarbon,
+                           List<TumbleResponse> monthlyTumbles, String Level) {
 
     public static HomeResponse from(User user) {
-        return new HomeResponse(user.getTumbles().calculateAnnualSaving(),
+        return new HomeResponse(
+            user.getTumbles().calculateAnnualSaving(),
             user.getTumbles().calculateAnnualCarbonSaving(),
             TumbleResponse.from(user.getTumbles()),
-            user.getLevel().name());
+            user.getLevel().name()
+        );
     }
 
-    public static class TumbleResponse {
-
-        private Long tumbleId;
-        private LocalDate createdDate;
-
-        public TumbleResponse(Long tumbleId, LocalDate createdDate) {
-            this.tumbleId = tumbleId;
-            this.createdDate = createdDate;
-        }
+    public record TumbleResponse(Long tumbleId, LocalDate createdDate) {
 
         public static List<TumbleResponse> from(Tumbles tumbles) {
             return tumbles.toResponse();
